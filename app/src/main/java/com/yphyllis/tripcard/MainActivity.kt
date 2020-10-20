7a10a1a92680c517e385d1576bd0bf3169c60715
022542140 <he.yang@evergrandeauto.com>
2020-10-20 17:42:56 +0800

    developing

--- app/src/main/java/com/yphyllis/tripcard/MainActivity.kt
+++ app/src/main/java/com/yphyllis/tripcard/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.yphyllis.tripcard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button = findViewById<Button>(R.id.btnGenerate)
+        button.setOnClickListener {
+            generateNow()
+        }
+    }
+
+    private fun generateNow() {
+        val mobile = findViewById<EditText>(R.id.editMobile).text.toString()
+        val routeArea = findViewById<EditText>(R.id.editRouteArea).text.toString()
+        if (mobile.isEmpty()) {
+            toast("手机号不能为空")
+            return
+        }
+        if (routeArea.isEmpty()) {
+            toast("途径地不能为空")
+            return
+        }
+    }
+
+    private fun toast(text: String) {
+        Toast.makeText(baseContext, text, Toast.LENGTH_LONG).show()
     }
 }
